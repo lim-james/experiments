13644efd98e48fa7cec290e388d5ff8d205b0722
James Lim <jameslimbj@gmail.com>
2024-10-10 10:02:46 +0800

    :sparkles: add variadic templates

--- cpp20/templates.cpp
+++ cpp20/templates.cpp
@@ -2,7 +2,15 @@
 
 template <typename T> T add(T a, T b) { return a + b; }
 
+template <typename T> void println(T a) { std::cout << a << '\n'; }
+template <typename T, typename... args> void println(T a, args... b) {
+  println(a);
+  println(b...);
+}
+
 int main() {
+  println(3);
+  println(4, 5, 6, 7);
   std::cout << "4 + 5 = " << add(4, 5) << '\n';
   return 0;
 }
