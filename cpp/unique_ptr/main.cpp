a25fc1a0de0bc317e24205da793d45eb276a5710
james <james@Frontiers-MacBook-Pro.local>
2025-10-13 17:43:57 +0800

    comply to rule of 5 for unique pointer

--- cpp/unique_ptr/main.cpp
+++ cpp/unique_ptr/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cassert>
+#include <utility>
 
 template<typename T>
 class unique_pointer {
@@ -10,6 +11,29 @@ private:
 public:
 
     unique_pointer(T* ptr): ptr_(ptr) {}
+
+    unique_pointer(const unique_pointer&) = delete; // copy constructor
+    void operator=(const unique_pointer&) = delete; // copy assignment
+
+    unique_pointer(unique_pointer&& up) { // move constructor
+        ptr_ = up.ptr_; 
+        up.ptr_ = nullptr;
+    }
+
+    unique_pointer& operator=(unique_pointer&& up) { // move assignment
+        ptr_ = up.ptr_; 
+        up.ptr_ = nullptr;
+        return this;
+    }
+
+    friend std::ostream& operator<<(
+        std::ostream& stream, 
+        const unique_pointer& up
+    ) {
+        stream << up.ptr_;
+        return stream;
+    }
+
 
     ~unique_pointer() {
         delete ptr_;
@@ -41,45 +65,72 @@ public:
     }
 };
 
+
 int main() {
+    std::cout << "sizeof = ";
+    {
+        assert(sizeof(unique_pointer<int>) == 8);
+    }
+    std::cout << "SUCCESS\n";
+
+
     std::cout << "=== Basic construction/destruction\n";
     {
-        unique_pointer<int> p1(new int(5));
-        //assert(p1);
-        assert(*p1 == 5);
+        unique_pointer<int> a(new int(5));
+        assert(*a == 5);
     }
 
 
     std::cout << "=== get\n";
     {
-        unique_pointer<int> p1(new int(5));
-        assert(p1.get() != nullptr);
+        unique_pointer<int> a(new int(5));
+        assert(a.get() != nullptr);
     }
 
 
     std::cout << "=== Release ownership\n";
     {
-        unique_pointer<int> p1(new int(5));
-        unique_pointer<int> p2(p1.release());
-        assert(p1.get() == nullptr);
-        assert(*p2 == 5);
+        unique_pointer<int> a(new int(5));
+        unique_pointer<int> b(a.release());
+        assert(a.get() == nullptr);
+        assert(*b == 5);
     }
 
 
     std::cout << "=== Replace managed object\n";
     {
-        unique_pointer<int> p1(new int(5));
-        p1.reset(new int(6));
-        assert(*p1 == 6);
+        unique_pointer<int> a(new int(5));
+        a.reset(new int(6));
+        assert(*a == 6);
     }
 
 
     std::cout << "=== Swap managed object\n";
     {
-        unique_pointer<int> p1(new int(5));
-        unique_pointer<int> p2(new int(7));
-        p1.swap(p2);
-        assert(*p1 == 7);
-        assert(*p2 == 5);
+        unique_pointer<int> a(new int(5));
+        unique_pointer<int> b(new int(7));
+        a.swap(b);
+        assert(*a == 7);
+        assert(*b == 5);
     }
+
+
+    std::cout << "=== copy object\n";
+    {
+        unique_pointer<int> a(new int(5));
+        // unique_pointer<int> b(a); // should be illegal
+        // unique_pointer<int> c = a; // should be illegal
+    }
+
+
+    std::cout << "=== move object\n";
+    {
+        unique_pointer<int> a(new int(5));
+        std::cout << "a[" << a << "] " << *a << '\n';
+        unique_pointer<int> b(std::move(a)); 
+        std::cout << "moved to -> b[" << b << "] " << *b << '\n';
+        unique_pointer<int> c = std::move(b);
+        std::cout << "moved to -> c[" << c << "] " << *c << '\n';
+    }
+
 }
