d01b8c2cfa8f6d8f9e9b7d115a6e74ad02aadc8a
james <james@Frontiers-MacBook-Pro.local>
2025-10-14 16:59:19 +0800

    attempting to move constants

--- cpp/move_semantics/main.cpp
+++ cpp/move_semantics/main.cpp
@@ -3,6 +3,47 @@
 #include <memory>
 #include <utility>
 
+class Object {
+private:
+    int i_ = 0; 
+
+public:
+
+    Object(int i): i_(i) {}
+    ~Object() {}
+
+    // copy 
+    Object(const Object& other) {
+        i_ = other.i_;
+    }
+
+    Object& operator=(const Object& other) {
+        i_ = other.i_;
+        return *this;
+    }
+
+    // move 
+    Object(Object&& other) {
+        if (this != &other)
+            i_ = other.i_;
+            other.i_ = 0; 
+    }
+
+
+    Object& operator=(Object&& other) {
+        if (this == &other)
+            return *this;
+        i_ = other.i_;
+        other.i_ = 0;
+        return *this;
+    }
+
+    int get() const {
+        return i_;
+    }
+
+};
+
 int main() {
     {
         std::string a = "hello";
@@ -21,7 +62,37 @@ int main() {
     {
         int* a = new int{1};
         int* b = std::move(a);
-        std::cout << *b << '\n';
+        std::cout << "a: " << a << ' ' << *a << '\n';
+        std::cout << "b: " << b << ' ' << *b << '\n';
+    }
+
+    {
+        int a = std::move(5);
+        std::cout << a << '\n';
+    }
+
+    {
+        Object a{5};
+        std::cout << "a: " << a.get() << '\n';
+        Object b{std::move(a)};
+        std::cout << "a: " << a.get() << '\n'; 
+        std::cout << "b: " << b.get() << '\n';
+    }
+
+    {
+        const Object a{5};
+        std::cout << "a: " << a.get() << '\n';
+        Object b{std::move(a)}; // fails silently, performs copy
+        std::cout << "a: " << a.get() << '\n'; 
+        std::cout << "b: " << b.get() << '\n';
+    }
+
+    {
+        const std::unique_ptr<int> a{new int{5}};
+        std::cout << "a: " << *a << '\n';
+        // a.reset(new int{10}); // not allowed
+        // not allowed copy constructor deleted
+        // const std::unique_ptr<int> b{std::move(a)};
     }
 
 }
